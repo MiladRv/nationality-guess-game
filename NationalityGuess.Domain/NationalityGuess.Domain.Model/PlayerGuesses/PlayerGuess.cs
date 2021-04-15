c2ea5bf94b072792a35ed99f1b2b91cb6df81581
milad rivandi <lvlilad.l2ivandi@gmail.com>
2021-04-16 00:02:54 +0430

    finish 'feature/adding-domains'

---
+++ NationalityGuess.Domain/NationalityGuess.Domain.Model/PlayerGuesses/PlayerGuess.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NationalityGuess.Domain.Model.UserGuesses
+{
+    public class PlayerGuess
+    {
+        public Guid Id { get; set; }
+        public Guid PlayerId { get; set; }
+        public int SelectedOptionId { get; set; }
+        public int CorrectOptionId { get; set; }
+
+        public bool IsTrue()
+        {
+            return SelectedOptionId == CorrectOptionId;
+        }
+
+
+    }
+}
