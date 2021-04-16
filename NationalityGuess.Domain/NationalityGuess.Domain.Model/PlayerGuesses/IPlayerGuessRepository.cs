8242f02f17a149fdb5fede1bbdb5a72ef2f55483
milad rivandi <lvlilad.l2ivandi@gmail.com>
2021-04-16 15:25:42 +0430

    implement player guess repository

---
+++ NationalityGuess.Domain/NationalityGuess.Domain.Model/PlayerGuesses/IPlayerGuessRepository.cs
@@ -0,0 +1,11 @@
+﻿using NationalityGuess.Domain.Model.UserGuesses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NationalityGuess.Domain.Model.PlayerGuesses
+{
+    public interface IPlayerGuessRepository : IRepository<PlayerGuess>
+    {
+    }
+}
